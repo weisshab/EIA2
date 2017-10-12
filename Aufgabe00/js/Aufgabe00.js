24e1e8daee7ec318c4a8cfb1fceccff34c0f5d91
basti <basti@LAPTOP-Q7NTVPEE.hfu-open.fu.hs-furtwangen.de>
2017-10-12 13:02:31 +0200

    Aufgabe0 1.0

--- Aufgabe00/js/Aufgabe00.js
+++ Aufgabe00/js/Aufgabe00.js
@@ -1,14 +1,14 @@
-document.addEventListener('Load', moin);
+document.addEventListener('Load', test);
 
 function moin(): void{
     let text:string;
-    let person = prompt("Wie heiﬂen Sie?", "Name");
+    let person = prompt("Wie hei√üen Sie?", "Name");
     
     if(person == null || person == ""){
         text = "Nameless";
     }
     else{
-        text += "Schˆn dich zu sehen" + person + "!";
+        text += "Sch√∂n dich zu sehen" + person + "!";
     }
     
     document.getElementById("text").innerHTML = text;
