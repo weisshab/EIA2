a2b657d607901d0ab0991e59d22868383a51daf7
basti <basti@LAPTOP-Q7NTVPEE.eduroam.hs-furtwangen.de>
2017-12-08 10:09:57 +0100

    comment

---
+++ Aufgabe08/js/main.js
@@ -0,0 +1,39 @@
+/*
+Aufgabe: 8
+Name: Bastian Weisshaar
+Matrikel: 256578
+Datum: 07.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var A8;
+(function (A8) {
+    window.addEventListener("load", input);
+    function input() {
+        let numbre = prompt("Gib eine Zahl zwischen 10 und 100 ein und schau was passiert! :)");
+        if (Number.isNaN(parseInt(numbre)) || (parseInt(numbre)) < 10 || (parseInt(numbre) > 100)) {
+            alert("Zahl ist nicht zwischen 10 und 100 mein Freund, versuche es nochmal");
+            input();
+        }
+        else {
+            //Random Gr��en sehen cooler aus
+            for (let i = 0; i < parseInt(numbre); i++) {
+                main(Math.random() * window.innerWidth, Math.random() * window.innerHeight, 10 + Math.random() * 70, Math.random() * 360);
+            }
+        }
+        function main(_left, _top, _size, _color) {
+            let left = Math.random() * window.innerWidth;
+            let top = Math.random() * window.innerHeight;
+            let square = document.createElement("div");
+            square.style.position = "absolute";
+            square.style.width = `${_size}px`;
+            square.style.height = `${_size}px`;
+            square.style.left = `${left}px`;
+            square.style.top = `${top}px`;
+            square.style.border = "2px solid black";
+            square.style.backgroundColor = "hsl(" + _color + ", 100%, 50%)";
+            document.body.appendChild(square);
+        }
+    }
+})(A8 || (A8 = {}));
+//# sourceMappingURL=main.js.map
---
+++ Aufgabe08/js/main.ts
@@ -0,0 +1,49 @@
+/*
+Aufgabe: 8
+Name: Bastian Weisshaar
+Matrikel: 256578
+Datum: 07.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace A8 {
+    window.addEventListener("load", input);
+
+    function input(): void {
+        let numbre: string = prompt("Gib eine Zahl zwischen 10 und 100 ein und schau was passiert! :)");
+
+        if (Number.isNaN(parseInt(numbre)) || (parseInt(numbre)) < 10 || (parseInt(numbre) > 100)) {
+            alert("Zahl ist nicht zwischen 10 und 100 mein Freund, versuche es nochmal");
+            input();
+        } else {
+            
+            //Random Größen sehen cooler aus
+            for (let i: number = 0; i < parseInt(numbre); i++) {
+                main(Math.random() * window.innerWidth, Math.random() * window.innerHeight, 10 + Math.random() * 70, Math.random() * 360);
+            }
+
+        }
+
+
+        function main(_left: number, _top: number, _size: number, _color: number): void {
+
+            let left: number = Math.random() * window.innerWidth;
+            let top: number = Math.random() * window.innerHeight;
+
+            let square: HTMLDivElement = document.createElement("div");
+            square.style.position = "absolute";
+            square.style.width = `${_size}px`;
+            square.style.height = `${_size}px`;
+
+            square.style.left = `${left}px`;
+            square.style.top = `${top}px`;
+
+            square.style.border = "2px solid black";
+
+            square.style.backgroundColor = "hsl(" + _color + ", 100%, 50%)";
+
+            document.body.appendChild(square);
+        }
+    }
+}
