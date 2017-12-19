4cb6a4c8828d6b6ea3c9d84886440c7a8d63834a
basti <basti@LAPTOP-Q7NTVPEE.hfu-open.fu.hs-furtwangen.de>
2017-12-19 12:25:22 +0100

    A10

---
+++ Aufgabe10/js/main.js
@@ -0,0 +1,14 @@
+/*
+Aufgabe: 10
+Name: Bastian Weisshaar
+Matrikel: 256578
+Datum: 12.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var A9;
+(function (A9) {
+    window.addEventListener("load", init);
+    function init() { }
+})(A9 || (A9 = {}));
+//# sourceMappingURL=main.js.map
---
+++ Aufgabe10/js/main.ts
@@ -0,0 +1,15 @@
+/*
+Aufgabe: 10
+Name: Bastian Weisshaar
+Matrikel: 256578
+Datum: 12.12.2017
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace A9 {
+    window.addEventListener("load", init);
+   
+    function init(): void{}
+}
+
