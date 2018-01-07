47855960041d1baf9d1029770d0aeb2c10866689
basti <basti@LAPTOP-Q7NTVPEE.fritz.box>
2018-01-07 17:03:13 +0100

    ^^

--- Aufgabe10/js/main.js
+++ Aufgabe10/js/main.js
@@ -6,9 +6,101 @@ Datum: 12.12.2017
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-var A9;
-(function (A9) {
+var A10;
+(function (A10) {
     window.addEventListener("load", init);
-    function init() { }
-})(A9 || (A9 = {}));
+    let baum = [];
+    let holder = [];
+    let height;
+    let licht = [];
+    let schmuck = [];
+    function init(_event) {
+        let fieldsets = document.getElementsByTagName("fieldset");
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+        }
+        let checkIt = document.getElementById("check");
+        checkIt.addEventListener("click", priceBox);
+    }
+    function handleChange(_event) {
+        let target = _event.target;
+        console.log("Changed " + target.name + " to " + target.value);
+        if (target.name == "Radiogroup") {
+            let currentTree = target.id;
+            let currentPrice = target.value;
+            baum[0] = currentTree;
+            baum[1] = currentPrice;
+            console.log(baum);
+        }
+        if (target.name == "Radiogroup2") {
+            let currentTree = target.id;
+            let currentPrice = target.value;
+            holder[0] = currentTree;
+            holder[1] = currentPrice;
+            console.log(holder);
+        }
+        if (target.name == "Stepper") {
+            let progress = document.getElementsByTagName("meter")[0];
+            progress.value = parseFloat(target.value);
+            height = progress.value;
+            console.log(height);
+        }
+        if (target.name == "Radiogroup1") {
+            let currentLights = target.id;
+            let currentPrice = target.value;
+            licht[0] = currentLights;
+            licht[1] = currentPrice;
+            console.log(licht);
+        }
+        if (this.id == "glamour") {
+            console.log("Changed " + target.name + " to " + target.checked);
+            let currentGlam = target.id;
+            let currentPrice = target.value;
+            schmuck.push(currentGlam, currentPrice);
+            console.log(schmuck);
+        }
+    }
+    function priceBox() {
+        let Price;
+        let priceTree = 0;
+        let priceHolder = 0;
+        let priceLight = 0;
+        let priceGlam = 0;
+        console.log(licht[1]);
+        if (baum[1] != undefined) {
+            priceTree += parseFloat(baum[1]);
+        }
+        else {
+            priceTree = 0;
+        }
+        if (holder[1] != undefined) {
+            priceHolder += parseFloat(holder[1]);
+        }
+        else {
+            priceHolder = 0;
+        }
+        if (licht[1] != undefined) {
+            priceLight += parseFloat(licht[1]);
+        }
+        for (let i = 1; i < schmuck.length; i += 2) {
+            if (schmuck[i] != undefined) {
+                priceGlam += parseFloat(schmuck[i]);
+                console.log("glam: " + priceGlam);
+            }
+        }
+        console.log("light: " + priceLight);
+        console.log("tree: " + priceTree);
+        console.log("Glam: " + priceGlam);
+        let total = priceTree + priceHolder + priceLight + priceGlam;
+        if (total != NaN) {
+            Price = "Gesamtbetrag: " + total + " Euro";
+            alert(`Baumart: ${priceTree + " Euro"}
+                \nHalterung: ${priceHolder + " Euro"}
+                \nBeleuchtung: ${priceLight + " Euro"}
+                \nSchmuck: ${priceGlam + " Euro"}
+                \n${Price}`);
+        }
+    }
+})(A10 || (A10 = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe10/js/main.ts
+++ Aufgabe10/js/main.ts
@@ -7,9 +7,118 @@ Datum: 12.12.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
-namespace A9 {
+namespace A10 {
     window.addEventListener("load", init);
-   
-    function init(): void{}
+
+    let baum: string[] = [];
+    let holder: string[] = [];
+    let height: number;
+    let licht: string[] = [];
+    let schmuck: string[] = [];
+
+    function init(_event: Event): void {
+
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+        }
+
+        let checkIt = document.getElementById("check");
+        checkIt.addEventListener("click", priceBox);
+
+    }
+
+    function handleChange(_event: Event): void {
+
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+
+        console.log("Changed " + target.name + " to " + target.value);
+
+        if (target.name == "Radiogroup") {
+            let currentTree: string = target.id;
+            let currentPrice: string = target.value;
+            baum[0] = currentTree;
+            baum[1] = currentPrice;
+            console.log(baum);
+        }
+        
+        if (target.name == "Radiogroup2") {
+            let currentTree: string = target.id;
+            let currentPrice: string = target.value;
+            holder[0] = currentTree;
+            holder[1] = currentPrice;
+            console.log(holder);
+        }
+
+        if (target.name == "Stepper") {
+            let progress: HTMLProgressElement = <HTMLProgressElement>document.getElementsByTagName("meter")[0];
+            progress.value = parseFloat(target.value);
+            height = progress.value;
+            console.log(height);
+        }
+
+        if (target.name == "Radiogroup1") {
+            let currentLights: string = target.id;
+            let currentPrice: string = target.value;
+            licht[0] = currentLights;
+            licht[1] = currentPrice;
+            console.log(licht);
+        }
+
+        if (this.id == "glamour") {
+            console.log("Changed " + target.name + " to " + target.checked);
+            let currentGlam: string = target.id;
+            let currentPrice: string = target.value;
+            schmuck.push(currentGlam, currentPrice)
+            console.log(schmuck);
+        }
+
+    }
+
+    function priceBox(): void {
+        let Price: string;
+        let priceTree: number = 0;
+        let priceHolder:number = 0;
+        let priceLight: number = 0;
+        let priceGlam: number = 0;
+
+        console.log(licht[1]);
+
+        if (baum[1] != undefined) {
+            priceTree += parseFloat(baum[1]);
+        } else { priceTree = 0; }
+        
+        if (holder[1] != undefined) {
+            priceHolder += parseFloat(holder[1]);
+        } else { priceHolder = 0; }
+
+        if (licht[1] != undefined) {
+            priceLight += parseFloat(licht[1]);
+        }
+
+        for (let i: number = 1; i < schmuck.length; i += 2) {
+            if (schmuck[i] != undefined) {
+                priceGlam += parseFloat(schmuck[i]);
+                console.log("glam: " + priceGlam);
+            }
+        }
+        console.log("light: " + priceLight);
+        console.log("tree: " + priceTree);
+        console.log("Glam: " + priceGlam);
+
+        let total: number = priceTree + priceHolder + priceLight + priceGlam;
+
+
+        if (total != NaN) {
+            Price = "Gesamtbetrag: " + total + " Euro";
+            alert(`Baumart: ${priceTree + " Euro"}
+                \nHalterung: ${priceHolder+ " Euro"}
+                \nBeleuchtung: ${priceLight + " Euro"}
+                \nSchmuck: ${priceGlam + " Euro"}
+                \n${Price}`);
+        }
+    }
 }
 
